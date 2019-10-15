7675ad7b78ae7f49412e91749cc49d3c4e4f5fb0
Leon Chen <a781282000@gmail.com>
2019-10-15 18:55:21 +0800

    練習switch

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
     <head>
-        <title>練習一下</title>
+        <title>購物網站</title>
     </head>
     <body>
         <form action = 'index2.php' method = 'post'>
-            <p><input type = 'radio' name = 'meal' value = '中式套餐'>中式套餐<img src = '/img/中式套餐.jpg' width = '200px' height = '150px'></p>
+            <p><input type = 'radio' name = 'meal' value = '中式套餐' checked>中式套餐<img src = '/img/中式套餐.jpg' width = '200px' height = '150px'></p>
             <p><input type = 'radio' name = 'meal' value = '日式套餐'>日式套餐<img src = '/img/日式套餐.jpg' width = '200px' height = '150px'></p>
             <p><input type = 'radio' name = 'meal' value = '西式套餐'>西式套餐<img src = '/img/西式套餐.jpeg' width = '200px' height = '150px' ></p>
             <p><input type = 'submit' value = '送出'></p> 
-        </form> 
+        </form>
     </body>  
 </html>
--- index2.php
+++ index2.php
@@ -1,14 +1,13 @@
 <?php
-if(isset($_REQUEST['meal'])){
-    if($_REQUEST['meal']=='中式套餐'){
-        echo '您選擇的為中式套餐。';
-    }else{
-        if($_REQUEST['meal']=='日式套餐'){
-          echo '您選擇的為日式套餐。';
-        }else{
-          echo '您選擇的為西式套餐。';
-        }
-    }
-}else{
-    echo '您未選擇任何餐點。';
+switch($_POST['meal']){
+case '中式套餐':
+    echo '春捲、煎餃、蛋花湯、炒飯、杏仁豆腐';
+    break;
+case '日式套餐':
+    echo '烤魚、燉菜、味噌湯、白飯、水果';
+    break;
+case '西式套餐':
+    echo '牛排、薯餅、密米濃湯、麵包、花椰菜';
+    break;        
 }
+echo '將稍後送達。';
