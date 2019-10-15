6ee5c0724c32c7506b048f10b9e8972d49d3072f
Leon Chen <a781282000@gmail.com>
2019-10-15 19:50:07 +0800

    for迴圈練習

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     </head>
     <body>
         <form action = 'index2.php' method = 'post'>
-            <p>選擇您要的座位類型</p>
-            <select name = 'seat'>
-                <option value = '自由座'>自由座</option>
-                <option value = '對號座'>對號座</option>
-                <option value = '商務艙'>商務艙</option>
+            <p>選擇您要的數量</p>
+            <select name = 'count'>
+                <?php
+                    for($i=0 ; $i<10 ; $i++){
+                        echo '<option value ="'. $i . '">' . $i .'</option>';
+                }
+                ?>
             </select>
             <input type = 'submit' value = '送出'> 
         </form>
