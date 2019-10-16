b65c84308699a9e4213e076c22cdb3ab3ea26e6d
Leon Chen <a781282000@gmail.com>
2019-10-16 15:38:47 +0800

    preg_match練習

--- index.php
+++ index.php
@@ -4,22 +4,9 @@
     </head>
     <body>
         <form action = 'index2.php' method = 'post'>
-            <p>選擇您要點餐的餐點</p>
-            <?php
-                $meal = ['紅茶',
-                         '綠茶',
-                         '奶茶',
-                         '三明治',
-                         '漢堡',
-                         '鬆餅',
-                         '鐵板麵',
-                         '薯餅'
-                ];
-                foreach($meal as $item){
-                    echo '<P><input type = "checkbox" name = "meal[]" value = "'.$item.'">'.$item.'</P>';
-            }
-            ?>
-            <p><input type = 'submit' value = '送出'></p>
+            <p>請輸入郵遞區號7碼，數字為0-9之間(不須輸入-)：</p>
+            <input type = 'text' name = 'postcode'>
+            <p><input type = 'submit' value = '確認'></p>
         </form>
     </body>  
 </html>
--- index2.php
+++ index2.php
@@ -1,9 +1,7 @@
 <?php
-if(isset($_POST['meal'])){
-    foreach($_POST['meal'] as $item){
-        echo '<p>'.$item.'</P>';
-    }
-    echo '以上餐點已開始準備，請稍候謝謝。';
+$postcode = $_POST['postcode'];
+if(preg_match('/^[0-9]{7}$/',$postcode)){
+    echo '您輸入之'.$postcode.'符合郵遞區號格式';
 }else{
-    echo'您未選擇任何餐點';
+    echo '您輸入之'.$postcode.'不符合郵遞區號格式，請重新確認。';
 }
