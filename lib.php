8f5a5fefcbe69944e69db14278cdac6215abf300
Leon Chen <a781282000@gmail.com>
2019-10-18 20:38:41 +0800

    建立lib.php

---
+++ lib.php
@@ -0,0 +1,32 @@
+<?php
+
+//取得../$i資料夾
+function thisProjectPath($i) {
+  $thisPhpPath = __File__;
+  $paths = explode('/', $thisPhpPath);
+  array_pop($paths);
+  $thisProjectPath = implode('/', $paths);
+  return $thisProjectPath . '/' .  $i;
+}
+//取得$i副檔名
+function extGet($i) {
+  $name = explode('.', $i);
+  return end($name);
+}
+//上傳檔案 postname, 資料夾, 新檔名
+function upload($i, $j, $k) {
+  if (is_uploaded_file($_FILES[$i]['tmp_name'])) {
+    if (!file_exists(thisProjectPath($j))) {
+      mkdir (thisProjectPath($j));
+    }
+  }
+  $originalName = basename($_FILES[$i]['name']);
+  $newName = thisProjectPath($j) . '/' . $k . '.' . extGet($originalName);
+  if (move_uploaded_file($_FILES[$i]['tmp_name'], $newName)) {
+    return '檔案上傳成功';
+  }else {
+    return '檔案上傳失敗';
+  }
+}
+
+
