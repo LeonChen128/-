dce24262bce9cc1bf0c38c784c399a7abf0ac7de
Leon Chen <a781282000@gmail.com>
2019-10-19 16:20:55 +0800

    html_table練習

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php
-
-include ('lib.php');
-
-$pdo = linkMysql('localhost', 'SHOP', 'root', '1234');
-
-
-foreach ($pdo->query('select * from Product') as $row) {
-  echo '<p>' . $row['id'] . '.' .  $row['name'] . ':' .  $row['price'] . '</p>';
-}  
-
-
+<html>
+  <head>
+    <title>這是個練習</title>
+  </head> 
+  <body>
+    <table>
+      <tr><th>商品編號</th>
+          <th>商品名稱</th>
+          <th>商品價格</th></tr>  
+      <?php
+      include ('lib.php');
+      $pdo = linkMysql('localhost', 'SHOP', 'root', '1234');
+      foreach ($pdo->query('select * from Product') as $row) {  
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($row['id']) . '</td>';
+        echo '<td>' . htmlspecialchars($row['name']) . '</td>';
+        echo '<td>' . htmlspecialchars($row['price']) . '</td>';
+        echo '</tr>';
+      }  
+      ?>
+    </table>
+  </body>  
+</html>
