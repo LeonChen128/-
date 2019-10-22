65ab55a04f9f5c6e199af9fde237e81480f2ae15
Leon Chen <a781282000@gmail.com>
2019-10-22 14:44:03 +0800

    修改資料庫內容練習

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <html>
   <head>
-    <title>這是個練習</title>
-  </head> 
+    <title>練習一下</title>
+  </head>
   <body>
-    <h1>建立商品資料</h1>
-    <form action='index2.php' method='post'>
-      <p>輸入商品名稱：</p>
-      <p><input type='text' name='name'></p>
-      <p>輸入商品價格(須為0-9阿拉伯數字)：</p>
-      <p><input type='text' name='price'></p>
-      <p><input type='submit' value='建立'></p>
-    </form>    
-  </body>  
+    <table>
+      <tr><th>商品編號</th><th>商品名稱</th><th>商品價格</th></tr>  
+      <?php
+      include('lib.php');
+      include('define.php');
+      
+      $pdo = linkMysql();
+      $sql = $pdo->prepare('SELECT * FROM Product');
+      $sql->execute();
+      foreach ($sql->fetchAll() as $row) {
+        echo '<tr>';
+        echo '<form action="index2.php" method="post">';
+        echo '<input type="hidden" name = "id" value="' . $row['id'] . '">';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td><input type="text" name="name" value="' . $row['name'] . '"></td>';
+        echo '<td><input type="text" name="price" value="' . $row['price'] . '"></td>';
+        echo '<td><input type="submit" value="修改"></td>';
+        echo '</form></tr>';
+      }
+      ?>
+    </table>
+  </body>
 </html>
--- index2.php
+++ index2.php
@@ -1,25 +1,17 @@
 <?php
 
 include('lib.php');
+include('define.php');
 
-$pdo = linkMysql('localhost', 'SHOP', 'root', '1234');
+$id    = $_POST['id'];
+$name  = $_POST['name'];
+$price = $_POST['price'];
 
-$name  = htmlspecialchars(trim($_POST['name']));
-$price = htmlspecialchars(trim($_POST['price']));
-
-if ($name =='' or $price =='') {
-  echo '商品名稱及價格輸入處不得空白，請重新輸入';
-  exit;
-}
-
-if (!preg_match('/[0-9]/', $price)) {
-  echo '價格欄位請輸入0-9之數字，請重新輸入';
-  exit;
-}
-
-$sql = 'insert into Product values(null,"' . $name . '", ' . $price . ')';
-
-if ($pdo->query($sql)) {
-  echo '商品新建成功';
+$pdo = linkMysql();
+$sql = $pdo->prepare('UPDATE Product SET name = ?, price = ? WHERE id = ?');
+if ($sql->execute([$name, $price, $id])) {
+  echo '修改成功';
+}else {
+  echo '修改失敗';
 }
 
