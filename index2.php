58c3ff186e2b6713ba83a667f959fcdc7649cf85
Leon Chen <a781282000@gmail.com>
2019-10-25 10:37:24 +0800

    資料庫CRUD練習

--- index2.php
+++ index2.php
@@ -4,9 +4,20 @@ include('lib.php');
 include('define.php');
 
 $pdo = linkMysql();
-$sql = $pdo->prepare('DELETE FROM Product WHERE id = ?');
-if ($sql->execute([$_REQUEST['id']])) {
-  echo '刪除成功';
-}else {
-  echo '刪除失敗';
+switch($_POST['command']) {
+  case 'insert':
+    $sql = $pdo->prepare('INSERT INTO Product VALUES(null, ?, ?)');
+    $sql->execute([$_POST['name'], $_POST['price']]);
+    echo '新增成功';
+    break;
+  case 'update':
+    $sql = $pdo->prepare('UPDATE Product SET name=?, price=? WHERE id=?');  
+    $sql->execute([$_POST['name'], $_POST['price'], $_POST['id']]);
+    echo '修改成功';
+    break;
+  case 'delete';
+    $sql = $pdo->prepare('DELETE FROM Product WHERE id=?');
+    $sql->execute([$_POST['id']]);
+    echo '刪除成功';
+    break;  
 }
