ac96ed1ce8a3e35ebe44bdc40157ecfd7bbad150
Leon Chen <a781282000@gmail.com>
2019-10-26 14:53:52 +0800

    session帳號登入練習

--- index.php
+++ index.php
@@ -3,37 +3,11 @@
     <title>練習一下</title>
   </head>
   <body>
-    <table>
-      <tr><th>商品編號</th><th>商品名稱</th><th>商品價格</th></tr>  
-      <?php
-      include('lib.php');
-      include('define.php');
-      $pdo = linkMysql();
-      $sql = $pdo->prepare('SELECT * FROM Product');
-      $sql->execute();
-      foreach ($sql->fetchAll() as $row) {
-        echo '<tr><form action="index2.php" method="post">';
-        echo '<input type="hidden" name="command" value="update">';
-        echo '<input type="hidden" name="id" value="' . $row['id'] .'">';
-        echo '<td>' . $row['id'] . '</td>';
-        echo '<td><input type="text" name="name" value="' . $row['name'] . '"></td>';
-        echo '<td><input type="text" name="price" value="' . $row['price'] . '"></td>';
-        echo '<td><input type="submit" value="確定修改"></td></form>';
-        echo '<form action="index2.php" method="post">';
-        echo '<input type="hidden" name="command" value="delete">';
-        echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-        echo '<td><input type="submit" value="確定刪除"></td></form></tr>';         
-      }
-      ?>
-      <form action="index2.php" method="post">
-        <tr>
-          <input type="hidden" name="command" value="insert">  
-          <td></td>
-          <td><input type="text" name="name"></td>
-          <td><input type="text" name="price"></td>
-          <td><input type="submit" value="確定新增"></td>
-        </tr>
-      </form>      
-    </table>
+    <form action="index2.php" method="post">
+      <h1>登入帳號</h1>  
+      <p>請輸入帳號：<input type="text" name="login"></p>
+      <p>請輸入密碼：<input type="password" name="password"></p>
+      <p><input type="submit" value="登入"></p>
+    </form>  
   </body>
 </html>
--- index2.php
+++ index2.php
@@ -3,21 +3,25 @@
 include('lib.php');
 include('define.php');
 
+session_start();
+unset($_SESSION['customer']);
+
 $pdo = linkMysql();
-switch($_POST['command']) {
-  case 'insert':
-    $sql = $pdo->prepare('INSERT INTO Product VALUES(null, ?, ?)');
-    $sql->execute([$_POST['name'], $_POST['price']]);
-    echo '新增成功';
-    break;
-  case 'update':
-    $sql = $pdo->prepare('UPDATE Product SET name=?, price=? WHERE id=?');  
-    $sql->execute([$_POST['name'], $_POST['price'], $_POST['id']]);
-    echo '修改成功';
-    break;
-  case 'delete';
-    $sql = $pdo->prepare('DELETE FROM Product WHERE id=?');
-    $sql->execute([$_POST['id']]);
-    echo '刪除成功';
-    break;  
+$sql = $pdo->prepare('SELECT * FROM Customer WHERE login=? AND password=?');
+$sql->execute([$_POST['login'], $_POST['password']]);
+foreach ($sql->fetchAll() as $row) {
+  $_SESSION['customer'] = [
+    'id' => $row['id'],
+    'name' => $row['name'],
+    'address' => $row['address'],
+    'login' => $row['login'],
+    'password' => $row['password']
+  ];
 }
+
+if (isset($_SESSION['customer'])) {
+  echo '歡迎回來，' . $_SESSION['customer']['name'] . '。';
+}else {
+  echo '輸入有錯';
+}
+
