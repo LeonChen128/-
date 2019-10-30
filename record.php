022a9ab6dacd8db2774b1b2a9069acf872c69c1f
Leon Chen <a781282000@gmail.com>
2019-10-30 18:11:21 +0800

    版面修飾

--- record.php
+++ record.php
@@ -15,17 +15,17 @@ echo '<p style="margin-left:20px;font-size:20px;">購買紀錄：</p>';
 foreach ($sql_purchase->fetchAll() as $row_purchase) {
   $sql_detail = $pdo->prepare('SELECT * FROM Purchase_Detail, Product WHERE purchase_id = ? AND product_id = Product.id');
   $sql_detail->execute([$row_purchase['id']]);
-  echo '<table class="record"><tr style="background-color:rgb(243, 173, 220);"><th>商品編號</th><th>商品名稱</th><th>價格</th><th>數量</th><th>小計</th></tr>';
+  echo '<table class="record"><tr style="background-color:rgb(81,161,180);"><th>商品編號</th><th>商品名稱</th><th>價格</th><th>數量</th><th>小計</th></tr>';
   $total = 0;
   foreach ($sql_detail->fetchAll() as $row_detail) {
-    echo '<tr><td>' . $row_detail['id'] . '</td>';
+    echo '<tr style="background:rgb(226,240,243);"><td>' . $row_detail['id'] . '</td>';
     echo '<td>' . $row_detail['name'] . '</td>';
     echo '<td>' . $row_detail['price'] . '</td>';
     echo '<td>' . $row_detail['count'] . '</td>';
-    echo '<td>' . $row_detail['price'] * $row_detail['count'] . '</td>';
+    echo '<td>' . $row_detail['price'] * $row_detail['count'] . '</td></tr>';
     $subtotal = $row_detail['price'] * $row_detail['count'];
     $total += $subtotal;
   }
-  echo '<tr><td>總計</td><td></td><td></td><td></td><td>' . $total . '</td></tr>';
+  echo '<tr style="background:rgb(241,148,169);"><td>總計</td><td></td><td></td><td></td><td>' . $total . '</td></tr>';
   echo '</table>';
 }
