4f06337a8e14f58a91e728d791229930a441c002
Leon Chen <a781282000@gmail.com>
2019-10-30 18:01:25 +0800

    購買紀錄顯示頁面

--- menu2.php
+++ menu2.php
@@ -1,7 +1,7 @@
 <div class="header">
   <a href="product1.php" class="word">商品</a>
   <a href="favorite-show.php" class="word">我的最愛</a>
-  <a href="" class="word">購買紀錄</a>
+  <a href="record.php" class="word">購買紀錄</a>
   <a href="car-show.php" class="word">購物車</a>
   <a href="buy.php" class="word">結帳</a>
   <a href="logout1.php" class="word">登出</a>
---
+++ record.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="lib.css">
+<body class="background"></body>
+
+<?php
+include('define.php');
+include('menu2.php');
+include('lib.php');
+
+session_start();
+
+$pdo = linkMysql();
+$sql_purchase = $pdo->prepare('SELECT * FROM Purchase WHERE customer_id = ? ORDER BY id DESC');
+$sql_purchase->execute([$_SESSION['customer']['id']]);
+echo '<p style="margin-left:20px;font-size:20px;">購買紀錄：</p>';
+foreach ($sql_purchase->fetchAll() as $row_purchase) {
+  $sql_detail = $pdo->prepare('SELECT * FROM Purchase_Detail, Product WHERE purchase_id = ? AND product_id = Product.id');
+  $sql_detail->execute([$row_purchase['id']]);
+  echo '<table class="record"><tr style="background-color:rgb(243, 173, 220);"><th>商品編號</th><th>商品名稱</th><th>價格</th><th>數量</th><th>小計</th></tr>';
+  $total = 0;
+  foreach ($sql_detail->fetchAll() as $row_detail) {
+    echo '<tr><td>' . $row_detail['id'] . '</td>';
+    echo '<td>' . $row_detail['name'] . '</td>';
+    echo '<td>' . $row_detail['price'] . '</td>';
+    echo '<td>' . $row_detail['count'] . '</td>';
+    echo '<td>' . $row_detail['price'] * $row_detail['count'] . '</td>';
+    $subtotal = $row_detail['price'] * $row_detail['count'];
+    $total += $subtotal;
+  }
+  echo '<tr><td>總計</td><td></td><td></td><td></td><td>' . $total . '</td></tr>';
+  echo '</table>';
+}
